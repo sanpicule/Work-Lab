20701887c167b3c33ac33352f4b75aa5045afafa
sanpicule <sannsi4444@gmail.com>
2023-08-31 20:14:09 +0900

    修正

--- next.config.js
+++ next.config.js
@@ -8,7 +8,9 @@ const nextConfig = {
       's3.us-west-2.amazonaws.com',
       'firebase.google.com',
       'd1tlzifd8jdoy4.cloudfront.net',
-      'rightcode.co.jp'
+      'rightcode.co.jp',
+      'buddy.works',
+      'ingg.dev'
     ], // 画像を提供するホスト名をここに追加
   },
 }
