6e4417b02bb4241078f8207a0faa9f51795faafd
Sanshiro Hikawa <106821913+sanpicule@users.noreply.github.com>
2025-04-20 18:40:30 +0900

    高さ調整 (#61)
    Co-authored-by: sanpicule <sannsi4444@gmail.com>

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -12,8 +12,8 @@ import ClickHereButton from './ClickHereButton'
 function Portfolio() {
   const customAnimate = useCustomAnimation()
   return (
-    <div className='w-full bg-[#eee] min-h-[100svh]'>
-      <div className='w-[80%] md:w-[60%] h-full min-h-[100svh] py-16 md:py-24 flex flex-col justify-center md:max-w-8xl mx-auto'>
+    <div className='w-full bg-[#eee] min-h-[100lvh]'>
+      <div className='w-[80%] md:w-[60%] h-full min-h-[100lvh] py-16 md:py-24 flex flex-col justify-center md:max-w-8xl mx-auto'>
         <SectionTitle title={siteConfig.portfolio} color='black' />
         <motion.div
           variants={customAnimate.scrollFadeInFromBottom}
@@ -27,8 +27,14 @@ function Portfolio() {
               key={portfolio.id}
               className='flex flex-col justify-center gap-2 rounded-2xl text-[#2e2e2e]'
             >
-              <p className={`${caveat.className} text-4xl font-bold`}>{i + 1}. {portfolio.title}</p>
-              <Link href={portfolio.url} target='_blank' className='w-full h-full p-4 rounded-md'>
+              <p className={`${caveat.className} text-4xl font-bold`}>
+                {i + 1}. {portfolio.title}
+              </p>
+              <Link
+                href={portfolio.url}
+                target='_blank'
+                className='w-full h-full p-4 rounded-md'
+              >
                 <div className='relative transition duration-500 w-full h-full cursor-pointer overflow-hidden rounded-md'>
                   <Image
                     src={portfolio.image}
@@ -40,7 +46,11 @@ function Portfolio() {
                 </div>
               </Link>
               <div>
-                <Link href={`/${portfolio.id}`} scroll={false} className='text-center mt-4'>
+                <Link
+                  href={`/${portfolio.id}`}
+                  scroll={false}
+                  className='text-center mt-4'
+                >
                   <ClickHereButton text='詳細を見る' bgcolor={'white'} />
                 </Link>
               </div>
